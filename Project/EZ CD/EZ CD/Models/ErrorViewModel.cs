a128e29d5438df4034e8ad78c85a5095c1c7b6d7
idan2402 <idan.chudner@gmail.com>
2020-08-12 17:21:18 +0300

    Created a new empty project in VS

---
+++ Project/EZ CD/EZ CD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EZ_CD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
