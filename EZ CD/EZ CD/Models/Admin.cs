f36cbc356cad33c684a2f287df6fbbe57d6d9d75
noam79 <60003899+noam79@users.noreply.github.com>
2020-08-16 17:45:14 +0300

    Create Admin.cs

---
+++ EZ CD/EZ CD/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZ_CD.Models
+{
+    public class Admin
+    {
+        int AdminId;
+        String Name;
+        String Username;
+        String Password;
+        String Email;
+    }
+}
