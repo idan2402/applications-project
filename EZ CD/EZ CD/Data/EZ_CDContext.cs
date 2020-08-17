bf1eb16353951728f46a48d678848aed1b9c8888
adirzaguri <adirzaguri10@gmail.com>
2020-08-17 17:53:28 +0300

    AdminController creating
    DB Admin table  connection

---
+++ EZ CD/EZ CD/Data/EZ_CDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EZ_CD.Models;
+
+namespace EZ_CD.Data
+{
+    public class EZ_CDContext : DbContext
+    {
+        public EZ_CDContext (DbContextOptions<EZ_CDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EZ_CD.Models.Admin> Admin { get; set; }
+    }
+}
