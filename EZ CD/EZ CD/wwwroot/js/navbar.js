be42eac976319abb5233ff253eb80c14ca8b2e47
noam79 <60003899+noam79@users.noreply.github.com>
2020-08-18 23:45:00 +0300

    Filters pop done (UI-wise)

---
+++ EZ CD/EZ CD/wwwroot/js/navbar.js
@@ -0,0 +1,25 @@
+﻿
+$(function () {
+
+    $('.main-dropdown-menu').on('click', function (event) {
+        event.stopPropagation();
+    });
+
+    $(window).on('click', function () {
+        $('.main-dropdown-menu').slideUp();
+    });
+
+});
+
+$(document).ready(function () {
+    $("#price").slider(
+        {
+            range: true,
+            min: 0,
+            max: 2000000,
+            change: function (e, ui) {
+                alert($("#price").slider("value", 0) + ' - ' + $("#price").slider("value", 1));
+            }
+        });
+    $("#price").slider("moveTo", 500000, 1);
+});
