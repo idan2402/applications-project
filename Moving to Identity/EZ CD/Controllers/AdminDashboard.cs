cd6cdd6f630e477a34f9c3d4fe548522e5aa2bf6
henashke <henashke21@gmail.com>
2020-08-21 15:25:57 +0300

    Fixed the Admin Dashboard
    Fixed the admin dashboard to work with the updated project and applied the admin layout to the CRUD pages of every model.

---
+++ Moving to Identity/EZ CD/Controllers/AdminDashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EZ_CD.Controllers
+{
+    public class AdminDashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
