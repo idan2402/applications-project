941314707645d3d2b2075f8412fef331951109d5
noam79 <60003899+noam79@users.noreply.github.com>
2020-08-21 19:53:29 +0300

    Small Changes
    fixed navbar search aesthetic, on disk edit/create pick from a list of genres, home & logo on navbar send to homepage.

---
+++ Moving to Identity/EZ CD/wwwroot/js/AdminPanel/createDisk.js
@@ -0,0 +1,9 @@
+﻿
+$(function () { // set the date added to the current date
+    var now = new Date();
+    var offset = now.getTimezoneOffset() * 60000;
+    var adjustedDate = new Date(now.getTime() - offset);
+    var formattedDate = adjustedDate.toISOString().substring(0, 16);
+    var datetimeField = document.getElementById("dateAdded");
+    datetimeField.value = formattedDate;
+});
