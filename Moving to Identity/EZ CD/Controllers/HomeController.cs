3a75093d5d09fa779fce16ef3abe5953738f4de3
henashke <henashke21@gmail.com>
2020-08-22 23:57:27 +0300

    Revert "Fixed the nullPointerException in the home view"
    This reverts commit 5d5a817dec0c2b5728eb8d6cd9e624ce241ec4ec.

--- Moving to Identity/EZ CD/Controllers/HomeController.cs
+++ Moving to Identity/EZ CD/Controllers/HomeController.cs
@@ -6,23 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EZ_CD.Models;
-using EZ_CD.Data;
 
 namespace EZ_CD.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly EZ_CD_DBContext _context;
-        public HomeController(ILogger<HomeController> logger, EZ_CD_DBContext context)
+
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View("Index", _context.Disk);
+            return View();
         }
 
         public IActionResult Privacy()
