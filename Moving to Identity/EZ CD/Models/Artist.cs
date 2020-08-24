3e9896c52dfd1d024f1718cee7985c08325ca130
noam79 <60003899+noam79@users.noreply.github.com>
2020-08-24 23:16:39 +0300

    Minor Fixes On Admin Dashboard
    gn

--- Moving to Identity/EZ CD/Models/Artist.cs
+++ Moving to Identity/EZ CD/Models/Artist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace EZ_CD.Models
 {
     public class Artist
     {
+        
         public int artistId { get; set; } // The artist ID
+        [DisplayName("Name")]
         public string name{ get; set; } // The artist name
+        [DisplayName("Birthday")]
         public DateTime birthday { get; set; } // His birthday
+        [DisplayName("Genre")]
         public string genre { get; set; } // The genre
+        [DisplayName("Country")]
         public string country { get; set; } // The country of origin
     }
 }
--- Moving to Identity/EZ CD/Models/Disk.cs
+++ Moving to Identity/EZ CD/Models/Disk.cs
@@ -3,21 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
+
 
 namespace EZ_CD.Models
 {
     public class Disk
     {
         public int diskId { get; set; } // The disk ID
+        [DisplayName("Price")]
         public double price { get; set; } // The price
+        [DisplayName("Name")]
         public string name { get; set; } // The name
+        [DisplayName("Release Date")]
         public DateTime date { get; set; } // Date of release
         public Artist Artist { get; set; } // The artist
+        [DisplayName("Genre")]
         public string genre { get; set; } // The genre
+        [DisplayName("Date Added To The Site")]
         public DateTime dateAdded { get; set; } // The date that the admin added the disk to the website
         public User Admin { get; set; } // The admin who added the disk to the DB
+        [DisplayName("Cover Image URL")]
         public string imagePath { get; set; } // The cover image url
-        public ICollection<Song> songs { get; set; }
+        [DisplayName("Featured Video URL")]
         public string featuredVideoUrl { get; set; } // the video URL
     }
 }
